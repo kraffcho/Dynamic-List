98a7f8cfbd3630757acbb5ac61ddadc9f5d48fbc
Stoyan <stoyan.kostadinov85@gmail.com>
2023-09-15 18:54:28 +0300

    feature/edit-search-ui
    This branch includes new functionalities such as item editing, task searching, and UI improvements to the existing Dynamic List React app. These additions enhance the user experience by offering more control and flexibility over the to-do list.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./styles.css"; // Import the CSS file
+import "./styles.css";
 
 function DynamicList() {
   const [items, setItems] = useState([
@@ -9,6 +9,9 @@ function DynamicList() {
   ]);
 
   const [newItemText, setNewItemText] = useState("");
+  const [editItem, setEditItem] = useState(null);
+  const [editedItemText, setEditedItemText] = useState("");
+  const [searchQuery, setSearchQuery] = useState("");
 
   const addItem = () => {
     if (newItemText.trim() === "") return;
@@ -22,29 +25,88 @@ function DynamicList() {
     setItems(updatedItems);
   };
 
+  const editItemText = (id) => {
+    const itemToEdit = items.find((item) => item.id === id);
+    setEditedItemText(itemToEdit.text);
+    setEditItem(id);
+  };
+
+  const saveEditedItem = (id) => {
+    const updatedItems = items.map((item) =>
+      item.id === id ? { ...item, text: editedItemText } : item
+    );
+    setItems(updatedItems);
+    setEditItem(null);
+  };
+
   return (
     <div className="container">
       <h2>Your To-Do List:</h2>
       <input
         type="text"
-        className="new-item"
-        placeholder="Type a new item..."
-        value={newItemText}
-        onChange={(e) => setNewItemText(e.target.value)}
+        className="search-input"
+        placeholder="Search..."
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
       />
-      <button className="cta" onClick={addItem}>
-        Add to List
-      </button>
       <ul className="list-container">
-        {items.map((item) => (
-          <li key={item.id} className="list-item">
-            {item.text}
-            <button className="cta" onClick={() => removeItem(item.id)}>
-              Remove
-            </button>
-          </li>
-        ))}
+        {items
+          .filter((item) =>
+            item.text.toLowerCase().includes(searchQuery.toLowerCase())
+          )
+          .map((item) => (
+            <li key={item.id} className="list-item">
+              {editItem === item.id ? (
+                <>
+                  <input
+                    type="text"
+                    className="new-item"
+                    value={editedItemText}
+                    onChange={(e) => setEditedItemText(e.target.value)}
+                  />
+                  <button
+                    className="cta"
+                    onClick={() => saveEditedItem(item.id)}
+                  >
+                    Save
+                  </button>
+                </>
+              ) : (
+                <>
+                  <div className="item-text">{item.text}</div>
+                  <button className="cta" onClick={() => editItemText(item.id)}>
+                    Edit
+                  </button>
+                  <button
+                    className="cta red"
+                    onClick={() => removeItem(item.id)}
+                  >
+                    Remove
+                  </button>
+                </>
+              )}
+            </li>
+          ))}
       </ul>
+      {items.length === 0 && searchQuery === "" && (
+        <p>Your list is empty. Add new items:</p>
+      )}
+      {items.length > 0 &&
+        items.filter((item) =>
+          item.text.toLowerCase().includes(searchQuery.toLowerCase())
+        ).length === 0 && <p>No results found for "{searchQuery}"</p>}
+      <div className="add-new-item">
+        <input
+          type="text"
+          className="new-item"
+          placeholder="Type a new item..."
+          value={newItemText}
+          onChange={(e) => setNewItemText(e.target.value)}
+        />
+        <button className="cta" onClick={addItem}>
+          Add
+        </button>
+      </div>
       <p>Total items on the list: {items.length}</p>
     </div>
   );
